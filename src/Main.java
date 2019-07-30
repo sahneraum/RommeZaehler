261900d63558ac87a6dd55663593293c2533b447
Marc Meierhof <marc@sahneraum.de>
2019-07-30 12:15:48 +0200

    Just a test

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+        // totalScore will be set to 0
+        int totalScore = 0;
+
+        //actualScore will be set to the input score
+        Scanner scanner = new Scanner();
+
+        int actualScore = scanner;
+
+        // The actualScore will be added to the totalScore
+        totalScore = totalScore + actualScore;
+
+        // print the totalScore
+        System.out.println(totalScore);
+    }
+
+}
